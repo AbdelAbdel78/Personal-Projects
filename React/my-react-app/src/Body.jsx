64a29e81a67d827ce60d996c9f3dd11e7a5c9b44
Abdelrahman Abdelgawad <66447803+AbdelAbdel78@users.noreply.github.com>
2024-08-12 06:35:12 -0400

    Learning React

---
+++ React/my-react-app/src/Body.jsx
@@ -0,0 +1,17 @@
+
+function Body(){
+
+    const varOne = "Foil"
+    const varTwo = "Epee"
+    const varThree = "Saber"
+
+    return (
+        <ol>
+            <li>{varOne}</li>
+            <li>{varTwo}</li>
+            <li>{varThree}</li>
+        </ol>
+    );
+}
+
+export default Body
---
+++ React/my-react-app/src/Footer.jsx
@@ -0,0 +1,12 @@
+
+function Footer(){
+
+    return(
+        <footer>
+            <hr/>
+            <p>&copy; {new Date().getFullYear()} My First React Website</p>
+        </footer>
+    );
+}
+
+export default Footer
